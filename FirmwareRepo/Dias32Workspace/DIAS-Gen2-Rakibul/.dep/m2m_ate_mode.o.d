b133e31d822fb40ad2941c56638e091e6eef1854
chowdhuryRakibul <chowdhury.rakibul@usask.ca>
2021-01-07 00:14:26 -0600

    added submodules
    ChibiOS-Contrib
    FirmwareRepo/Dias32Workspace/gen2Practice

--- FirmwareRepo/Dias32Workspace/DIAS-Gen2-Rakibul/.dep/m2m_ate_mode.o.d
+++
@@ -1,1 +0,0 @@
-build/obj/m2m_ate_mode.o: winc1500/driver/source/m2m_ate_mode.c
--- FirmwareRepo/Dias32Workspace/DIAS-Gen2-Rakibul/.dep/vectors.o.d
+++
@@ -1,8 +0,0 @@
-build/obj/vectors.o: \
- ../../../ChibiOSRepo/chibios191/os/common/startup/ARMCMx/compilers/GCC/vectors.S \
- ../../../ChibiOSRepo/chibios191/os/common/startup/ARMCMx/devices/STM32F4xx/cmparams.h \
- ../../../ChibiOSRepo/chibios191/os/hal/boards/OLIMEX_STM32_E407/board.h
-
-../../../ChibiOSRepo/chibios191/os/common/startup/ARMCMx/devices/STM32F4xx/cmparams.h:
-
-../../../ChibiOSRepo/chibios191/os/hal/boards/OLIMEX_STM32_E407/board.h:
